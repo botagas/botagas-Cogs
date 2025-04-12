6493f1f3f308508c98e86d0c90ead3c386197741
botagas <49989644+botagas@users.noreply.github.com>
2025-04-12 22:19:19 +0300

    Create format.py

---
+++ captcha/format.py
@@ -0,0 +1,10 @@
+import discord
+
+def format_message(template: str, member: discord.Member) -> str:
+    return template.format(
+        mention=member.mention,
+        name=member.display_name,
+        username=member.name,
+        guild=member.guild.name,
+        id=member.id,
+    )
