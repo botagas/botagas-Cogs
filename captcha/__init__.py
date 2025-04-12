b721d7c1cdf72ad181787e35ffb4d57f46df1592
botagas <49989644+botagas@users.noreply.github.com>
2025-04-12 23:47:52 +0300

    Update __init__.py

--- captcha/__init__.py
+++ captcha/__init__.py
@@ -21,6 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+from redbot.core.bot import Red
 from .core import Captcha
 
 async def setup(bot: Red) -> None:
