a87a7406210799e0f2055ecd5ed9fa8bc21768e7
Patrick <49989644+botagas@users.noreply.github.com>
2025-04-12 14:58:20 +0300

    Create views.py

---
+++ captcha/views.py
@@ -0,0 +1,34 @@
+# views.py
+import discord
+from typing import Optional
+
+class CaptchaVerifyButton(discord.ui.View):
+    def __init__(self, cog: Any, timeout: Optional[float] = None):
+        super().__init__(timeout=timeout)
+        self.cog = cog
+
+    @discord.ui.button(
+        label="Verify", style=discord.ButtonStyle.success, custom_id="captcha_verify"
+    )
+    async def verify(self, interaction: discord.Interaction, button: discord.ui.Button):
+        member = interaction.user
+
+        if member.bot:
+            return
+
+        guild_config = await self.cog.config.guild(interaction.guild).all()
+        channel_id = guild_config["channel"]
+
+        if not channel_id:
+            return await interaction.response.send_message(
+                "Verification channel not configured.", ephemeral=True
+            )
+
+        channel = interaction.guild.get_channel(channel_id)
+        if not isinstance(channel, discord.TextChannel):
+            return await interaction.response.send_message(
+                "Verification channel is invalid.", ephemeral=True
+            )
+
+        await interaction.response.defer(ephemeral=True)
+        await self.cog.begin_captcha_flow(member, channel)
